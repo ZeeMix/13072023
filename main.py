ddacf1eeb0904010420e9e990c6aa01c8157cc5e
ZooMix <salnikovsanya2002@gmail.com>
2023-07-13 22:38:18 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# print('Hello world!', end=' ')
+
+# name = 'Alex'
+
+# name = input('Введите имя: ')
+# surname = input('Введите фамилию: ')
+# print('Здравствуйте, ' + name + ' ' + surname)
+# print('Здравствуйте,', name, surname)
+# print(f'Здравствуйте, {name} {surname}')
+
+# a = 'Hello'
+# print(type(a), a)
+# b = 13
+# print(type(b), b)
+# c = 3.45
+# print(type(c), c)
+# d = True
+# print(type(d), d)
+
+# a = 15
+# b = 7
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# print(a < b)
+# print(a > b)
+# print(a <= b)
+# print(a >= b)
+# print(a == b)
+# print(a != b)
+
+
+# a = int(input('Введите число: '))
+# if a > 0:
+#     if a % 2 == 0:
+#         print(f'Число {a} четное положительное!')
+#     else:
+#         print(f'Число {a} нечетное положительное!')
+# elif a == 0:
+#     print('Число равно 0')
+# else:
+#     if a % 2 == 0:
+#         print(f'Число {a} четное отрицательное!')
+#     else:
+#         print(f'Число {a} нечетное отрицательное!')
